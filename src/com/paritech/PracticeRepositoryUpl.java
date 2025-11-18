a282de08a507625e2fe2fe9e59b6c70b88e48d3e
shoebkhan912928 <shoebkhan912928@gmail.com>
2025-11-18 15:53:32 +0530

    second commit

--- src/com/paritech/PracticeRepositoryUpl.java
+++ src/com/paritech/PracticeRepositoryUpl.java
@@ -5,6 +5,7 @@ public class PracticeRepositoryUpl {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a paractice of upload git repository ");
+		System.out.println("This is a second commit");
 	}
 
 }
